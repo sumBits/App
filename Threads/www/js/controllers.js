2000b4ca46055203f80092a776fce73dc23a4155
mathukumillia <mathukumillia@s.dcsdk12.org>
2015-03-05 18:08:14 -0700

    refresh now retrieves based on current location

--- Threads/www/js/controllers.js
+++ Threads/www/js/controllers.js
@@ -1,76 +1,94 @@
 angular.module('starter.controllers', [])
 
 .controller('UserThreadChatCtrl', function ($scope) {
-   
+
 })
 
 .controller('UserThreadCtrl', function ($scope, $stateParams) {
-    
+
 })
 
-.controller('NearbyThreadCtrl', function($scope, NearbyThreadsGetter) {
-    $scope.posts = NearbyThreadsGetter.getNearby({"latitude":39.25,"longitude":-104.95});
+.controller('NearbyThreadCtrl', function ($scope, NearbyThreadsGetter) {
+    $scope.posts = NearbyThreadsGetter.getNearby({
+        "latitude": 39.25,
+        "longitude": -104.95
+    });
 
-    NearbyThreadsGetter.getNearby({"latitude":39.25,"longitude":-104.95}).then(function(response) {
+    NearbyThreadsGetter.getNearby({
+        "latitude": 39.25,
+        "longitude": -104.95
+    }).then(function (response) {
         console.log(response);
         console.log(response[0])
         $scope.posts = response;
     });
 
-    $scope.nearbyRefresh = function() {
-        NearbyThreadsGetter.nearbyRefresh({"latitude":39.25,"longitude":-104.95})
-        .then(function(response) {
-            $scope.posts = response;
-        }).finally(function() {
-            $scope.$broadcast('scroll.refreshComplete');
+    $scope.nearbyRefresh = function () {
+        navigator.geolocation.getCurrentPosition(function (position) {
+            $scope.currentLocation = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+            console.log("latitude: ", $scope.currentLocation.k);
+            console.log("longitude: ", $scope.currentLocation.D);
+
+            NearbyThreadsGetter.nearbyRefresh({
+                    "latitude": $scope.currentLocation.k,
+                    "longitude": $scope.currentLocation.D
+                })
+                .then(function (response) {
+                    $scope.posts = response;
+                }).finally(function () {
+                    $scope.$broadcast('scroll.refreshComplete');
+                });
+
+        }, function (error) {
+            alert(error);
         });
-    }
+    };
 })
 
 .controller('AccountCtrl', function ($scope, UserFactory) {
-    
+
     $scope.hideAllSignins = false;
     $scope.showSignupForm = false;
     $scope.showLoginForm = false;
     $scope.showFBLogin = false;
-    
+
     // initialization
     UserFactory.getUser().then(function success(response) {
         $scope.user = response.data;
     });
-    
-    $scope.toggleSignupForm = function(){
+
+    $scope.toggleSignupForm = function () {
         $scope.showSignupForm = !($scope.showSignupForm);
         $scope.showLoginForm = false;
         $scope.showFBLogin = false;
     }
-    
-    $scope.toggleLoginForm = function(){
+
+    $scope.toggleLoginForm = function () {
         $scope.showLoginForm = !($scope.showLoginForm);
         $scope.showSignupForm = false;
         $scope.showFBLogin = false;
     }
-    
-    $scope.toggleFBLogin = function(){
+
+    $scope.toggleFBLogin = function () {
         $scope.showFBLogin = !($scope.showFBLogin);
         $scope.showSignupForm = false;
         $scope.showLoginForm = false;
     }
-    
-    $scope.signup = function(usn,age,em,pwd){
-        UserFactory.signup(usn,age,em,pwd).then(function success(response){
+
+    $scope.signup = function (usn, age, em, pwd) {
+        UserFactory.signup(usn, age, em, pwd).then(function success(response) {
             $scope.signup.usn = null;
             $scope.signup.age = null;
             $scope.signup.em = null;
             $scope.signup.pwd = null;
             console.log(response);
-            $scope.login(em,pwd);
-        }, function handleError(response){
+            $scope.login(em, pwd);
+        }, function handleError(response) {
             alert('Error: ' + response.data);
         });
     }
-    
-    $scope.login = function(em,pwd){
+
+    $scope.login = function (em, pwd) {
         UserFactory.login(em, pwd).then(function success(response) {
             UserFactory.getUser().then(function success(response) {
                 $scope.user = response.data.user;
@@ -81,17 +99,17 @@ angular.module('starter.controllers', [])
             $scope.hideAllSignins = true;
         }, function handleError(response) {
             alert('Error: ' + response.data);
-        });       
+        });
     }
-    
-    $scope.FBLogin = function(em, pwd){
-    
+
+    $scope.FBLogin = function (em, pwd) {
+
     }
-    
-    $scope.logout = function(){
+
+    $scope.logout = function () {
         UserFactory.logout();
         $scope.user = null;
         $scope.hideAllSignins = false;
     }
-    
+
 });
